5938a9b19c52a8492972bb217d795da4d4ccc48c
jyin <jyin@st.ug.edu.gh>
2022-10-07 03:39:12 +0100

    added stack ds with array

---
+++ stack_with_array.py
@@ -0,0 +1,21 @@
+size = 3
+data = [0] *(size)
+
+top = -1
+
+def push(x):
+    global top
+    if top >= size-1:
+        print("Stack Overflow")
+    else:
+        top = top +1
+        data[top] = x
+
+push("ham")
+push("egg")
+push("spam")
+
+print(data[0: top+1])
+
+push("new")
+push("another")
